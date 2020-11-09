c9b77d4736c46ce26a8bcc27be89b14bdc09afa1
Farhad Hossain <farhadhossain0085@gmail.com>
2020-11-09 17:50:50 +0600

    post models added

--- blog/models.py
+++ blog/models.py
@@ -25,3 +25,45 @@ class UserProfile(models.Model):
         """String representation for this model"""
 
         return self.user.username
+
+
+
+class Post(models.Model):
+    """Blog Post model"""
+    
+    title = models.CharField(max_length=400)
+    body = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+
+    # relationship
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    picture = models.ManyToManyField('PostPicture')    
+
+    def __str__(self):
+        """String representation for this model"""
+
+        return self.title
+
+
+class PostPicture(models.Model):
+    """Post Picture model"""
+    
+    picture = models.ImageField(upload_to=PICTURE_UPLOAD_TO)
+
+    def __str__(self):
+        """String representation for this model"""
+
+        return self.picture.url
+
+
+class PostView(models.Model):
+    """Model for storing Post views"""
+    
+    view = models.IntegerField(default=0)
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """String representation for this model"""
+
+        return f"{self.view}"
--- blog/signals.py
+++ blog/signals.py
@@ -1,15 +1,24 @@
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from .models import UserProfile
+from .models import UserProfile, PostView, Post
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    """Create an user profile once the user created"""
+    """Create a user profile once the user created"""
 
     if created:
         UserProfile.objects.create(
             birth_date='1900-01-01',
             user=instance
         )
-        
+
+
+@receiver(post_save, sender=Post)
+def create_post_view(sender, instance, created, **kwargs):
+    """Create a post view item for the post once the post created"""
+
+    if created:
+        PostView.objects.create(
+            post=instance
+        )
