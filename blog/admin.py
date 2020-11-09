722a89a8175a56e4abbd1e09977ae45cb3314697
Farhad Hossain <farhadhossain0085@gmail.com>
2020-11-09 18:08:13 +0600

    model admin added

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Post, PostPicture, PostView
 
 
 class UserProfileAdmin(admin.ModelAdmin):
@@ -23,4 +23,33 @@ class UserProfileAdmin(admin.ModelAdmin):
     get_short_bio.short_description = "Bio"
     get_age.short_description = "Age"
 
+
+class PostAdmin(admin.ModelAdmin):
+    """Customize admin interface for this model"""
+
+    list_display = ['title', 'get_post_view', 'user', 'date_created', 'date_updated', 'get_short_body']
+
+    def get_short_body(self, instance):
+        """Get short body for a post"""
+        
+        return instance.body[:200]
+
+    def get_post_view(self, instance):
+        """Get post view for a post"""
+        
+        return instance.postview.view
+
+    get_short_body.short_description = "Body"
+    get_post_view.short_description = "Views"
+
+
+class PostViewAdmin(admin.ModelAdmin):
+    """Customize admin interface for this model"""
+
+    list_display = ['post', 'view']
+
+
 admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(PostPicture)
+admin.site.register(PostView, PostViewAdmin)
